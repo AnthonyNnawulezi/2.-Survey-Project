fa3958bebe6fe6e1040f5ede82a789da5c2f8672
antonsprime <evansanton58@gmail.com>
2026-06-10 23:57:02 +0100

    made dashborad and surveys protected routes

--- React/src/views/Dashboard.jsx
+++ React/src/views/Dashboard.jsx
@@ -1,6 +1,11 @@
+import { Navigate } from "react-router-dom";
 import PageComponent from "../Components/PageComponent";
+import { useStateContext } from "../Context/Context";
 
 function Dashboard({ children, buttons }) {
+    const { token } = useStateContext();
+    if (token) return <Navigate to={"/dashboard"} />;
+
     return (
         <PageComponent
             title="Dashboard"
--- React/src/views/Surveys.jsx
+++ React/src/views/Surveys.jsx
@@ -1,6 +1,12 @@
+import { Navigate } from "react-router-dom";
 import PageComponent from "../Components/PageComponent";
+import { useStateContext } from "../Context/Context";
 
 function Surveys({ children, buttons }) {
+    const { token } = useStateContext();
+
+    if (!token) return <Navigate to={"/login"} />;
+
     return (
         <PageComponent
             title="Surveys"
