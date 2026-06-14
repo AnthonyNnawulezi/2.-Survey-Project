128fbba3596c7e5323aea3339d7c14f46a3e9b99
antonsprime <evansanton58@gmail.com>
2026-06-14 19:29:59 +0100

    added password confirmation

--- React/src/views/Signup.jsx
+++ React/src/views/Signup.jsx
@@ -82,6 +82,25 @@ export default function Signup() {
                                 />
                             </div>
                         </div>
+                        <div>
+                            <div className="flex items-center justify-between">
+                                <label
+                                    htmlFor="confirm_password"
+                                    className="block font-medium text-gray-100 text-sm/6"
+                                >
+                                    Password
+                                </label>
+                            </div>
+                            <div className="mt-2">
+                                <input
+                                    id="confirm_password"
+                                    name="confirm_password"
+                                    type="password"
+                                    required
+                                    className="block w-full rounded-md bg-white/5 px-3 py-1.5 text-base text-white outline-1 -outline-offset-1 outline-white/10 placeholder:text-gray-500 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-500 sm:text-sm/6"
+                                />
+                            </div>
+                        </div>
 
                         <div>
                             <button
